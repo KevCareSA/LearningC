9cdd875fe4d7212fee469dfe2b34f3ba886ff09b
KevCareSA <kev.mkbn2@gmail.com>
2023-04-16 20:18:39 -0700

    comments update

---
+++ 00-Comments/comments.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+Running code with VSCode
+Its so different from unix 
+I like it.
+*/
+
+
+int main()
+{
+    //printf("comment out this printf\n");
+    
+    printf("Hello World\n");
+
+    return 0;
+}
